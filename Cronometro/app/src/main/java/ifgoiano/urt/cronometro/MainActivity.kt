78e597ef52050fbf3c7fd47287249b3898dd9149
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2024-09-24 07:39:47 -0300

    24-09-2024 v2

--- Cronometro/app/src/main/java/ifgoiano/urt/cronometro/MainActivity.kt
+++ Cronometro/app/src/main/java/ifgoiano/urt/cronometro/MainActivity.kt
@@ -1,12 +1,21 @@
 package ifgoiano.urt.cronometro
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var segundos: Int = 0
+    private var rodando: Boolean = false
+    private val handler = Handler(Looper.getMainLooper())
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,42 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        runTimer()
+
+    }
+
+    fun onClickStart(view: View) {
+        rodando = true
+    }
+
+    fun onClickStop(view: View) {
+        rodando = false
+    }
+
+    fun onClickReset(view: View) {
+        rodando = false
+        segundos = 0
+    }
+
+    private fun runTimer() {
+        val timeView = findViewById<TextView>(R.id.time_view)
+
+        handler.post(object : Runnable {
+            override fun run() {
+                val horas = segundos / 3600
+                val minutos = (segundos % 3600) / 60
+                val secs = segundos % 60
+                val time = String.format("%02d:%02d:%02d", horas, minutos, secs)
+
+                timeView.text = time
+
+                if (rodando) {
+                    segundos++
+                }
+
+                handler.postDelayed(this, 1000)
+            }
+        })
     }
 }
