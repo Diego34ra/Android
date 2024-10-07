f5d0e94c5d06edebf203fc56bacbed1c27c3cdf0
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2024-10-07 10:07:35 -0300

    07-10-2024 v1

--- Cronometro/app/src/main/java/ifgoiano/urt/cronometro/TemporizadorActivity.kt
+++ Cronometro/app/src/main/java/ifgoiano/urt/cronometro/TemporizadorActivity.kt
@@ -1,17 +1,16 @@
 package ifgoiano.urt.cronometro
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.NumberPicker
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class TemporizadorActivity : AppCompatActivity() {
     private var tempoRestante: Long = 0
@@ -23,6 +22,8 @@ class TemporizadorActivity : AppCompatActivity() {
     private lateinit var npSeconds: NumberPicker
     private lateinit var timeView: TextView
     private lateinit var linearLayout: LinearLayout // LinearLayout que contém os NumberPickers
+    private lateinit var btnPausarRetomar: Button // Botão que alterna entre pausar e retomar
+    private lateinit var btnIniciarRestaurar: Button // Botão para iniciar ou restaurar o temporizador
     private val TAG: String = "si-urutai"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,6 +36,8 @@ class TemporizadorActivity : AppCompatActivity() {
         npSeconds = findViewById(R.id.np_seconds)
         timeView = findViewById(R.id.time_view_temporizador)
         linearLayout = findViewById(R.id.linearLayout)
+        btnPausarRetomar = findViewById(R.id.btn_resume_temporizador)
+        btnIniciarRestaurar = findViewById(R.id.btn_start_temporizador)
 
         // Configurar valores mínimos e máximos para os NumberPickers
         npHours.minValue = 0
@@ -46,18 +49,26 @@ class TemporizadorActivity : AppCompatActivity() {
         npSeconds.minValue = 0
         npSeconds.maxValue = 59
 
-        // Botão para iniciar o temporizador
-        findViewById<Button>(R.id.btn_start_temporizador).setOnClickListener {
-            if (!temporizadorRodando) {
+        // Inicialmente, esconder o botão de pausar/retomar
+        btnPausarRetomar.visibility = Button.GONE
+
+        // Botão para iniciar ou restaurar o temporizador
+        btnIniciarRestaurar.setOnClickListener {
+            if (!temporizadorRodando && !temporizadorPausado) {
                 iniciarTemporizador()
+            } else {
+                restaurarTemporizador()
             }
         }
 
-        // Botão para voltar ao cronômetro
-//        findViewById<Button>(R.id.btn_open_cronometro).setOnClickListener {
-//            val intent = Intent(this, CronometroActivity::class.java)
-//            startActivity(intent)
-//        }
+        // Botão para pausar ou retomar o temporizador
+        btnPausarRetomar.setOnClickListener {
+            if (temporizadorRodando) {
+                pausarTemporizador()
+            } else if (temporizadorPausado) {
+                retomarTemporizador()
+            }
+        }
     }
 
     fun iniciarTemporizador() {
@@ -69,8 +80,12 @@ class TemporizadorActivity : AppCompatActivity() {
 
         if (tempoRestante > 0) {
             temporizadorRodando = true
+            temporizadorPausado = false // Certifique-se de que o temporizador não está pausado
             linearLayout.visibility = LinearLayout.GONE
             timeView.visibility = TextView.VISIBLE
+            btnPausarRetomar.visibility = Button.VISIBLE // Exibir o botão de pausar/retomar
+            btnIniciarRestaurar.text = "Restaurar"
+            btnPausarRetomar.text = "Pausar" // Definir o texto corretamente para "Pausar"
 
             countDownTimer = object : CountDownTimer(tempoRestante, 1000) {
                 override fun onTick(millisUntilFinished: Long) {
@@ -83,6 +98,10 @@ class TemporizadorActivity : AppCompatActivity() {
                     timeView.text = "00:00:00"
                     linearLayout.visibility = LinearLayout.VISIBLE
                     timeView.visibility = TextView.GONE
+                    btnPausarRetomar.visibility = Button.GONE // Esconder o botão de pausar/retomar
+                    btnIniciarRestaurar.text = "Iniciar" // Alterar texto do botão para "Iniciar"
+
+                    exibirMensagemTemporizadorFinalizado() // Exibir mensagem ao finalizar o temporizador
                     Log.i(TAG, "Temporizador finalizado!")
                 }
             }.start()
@@ -91,37 +110,54 @@ class TemporizadorActivity : AppCompatActivity() {
         }
     }
 
-    fun onClickPausarTemporizador(view: View) {
+    fun restaurarTemporizador() {
+        countDownTimer.cancel()
+        temporizadorRodando = false
+        temporizadorPausado = false
+        tempoRestante = 0
+        atualizarTimerUI(0)
+        linearLayout.visibility = LinearLayout.VISIBLE
+        timeView.visibility = TextView.GONE
+        btnPausarRetomar.visibility = Button.GONE // Esconder o botão de pausar/retomar ao restaurar
+        btnIniciarRestaurar.text = "Iniciar"
+        Log.i(TAG, "Temporizador restaurado para 0.")
+    }
+
+    fun pausarTemporizador() {
         if (temporizadorRodando) {
             countDownTimer.cancel()
             temporizadorRodando = false
             temporizadorPausado = true
+            btnPausarRetomar.text = "Retomar"
             Log.i(TAG, "Temporizador pausado.")
         }
     }
 
-    fun onClickRetomarTemporizador(view: View) {
+    fun retomarTemporizador() {
         if (temporizadorPausado) {
-            iniciarTemporizadorComTempoRestante()
+            temporizadorRodando = true
             temporizadorPausado = false
+            btnPausarRetomar.text = "Pausar" // Alterar para "Pausar" após retomar
+
+            countDownTimer = object : CountDownTimer(tempoRestante, 1000) {
+                override fun onTick(millisUntilFinished: Long) {
+                    tempoRestante = millisUntilFinished
+                    atualizarTimerUI(millisUntilFinished)
+                }
+
+                override fun onFinish() {
+                    temporizadorRodando = false
+                    timeView.text = "00:00:00"
+                    linearLayout.visibility = LinearLayout.VISIBLE
+                    timeView.visibility = TextView.GONE
+                    btnPausarRetomar.visibility = Button.GONE
+                    btnIniciarRestaurar.text = "Iniciar"
+
+                    exibirMensagemTemporizadorFinalizado() // Exibir mensagem ao finalizar o temporizador
+                    Log.i(TAG, "Temporizador finalizado!")
+                }
+            }.start()
         }
-    }
-
-    fun iniciarTemporizadorComTempoRestante() {
-        countDownTimer = object : CountDownTimer(tempoRestante, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                tempoRestante = millisUntilFinished
-                atualizarTimerUI(millisUntilFinished)
-            }
-
-            override fun onFinish() {
-                temporizadorRodando = false
-                timeView.text = "00:00:00"
-                linearLayout.visibility = LinearLayout.VISIBLE // Mostrar os NumberPickers novamente
-                timeView.visibility = TextView.GONE // Esconder o tempo restante
-                Log.i(TAG, "Temporizador finalizado!")
-            }
-        }.start()
     }
 
     private fun atualizarTimerUI(millisUntilFinished: Long) {
@@ -131,12 +167,16 @@ class TemporizadorActivity : AppCompatActivity() {
         timeView.text = String.format("%02d:%02d:%02d", horas, minutos, segundos)
     }
 
+    private fun exibirMensagemTemporizadorFinalizado() {
+        Toast.makeText(this, "Temporizador finalizado!", Toast.LENGTH_SHORT).show()
+    }
+
     override fun onStop() {
         super.onStop()
         if (temporizadorRodando) {
             countDownTimer.cancel()
             temporizadorRodando = false
-            Log.i(TAG, "Temporizador pausado.")
+            Log.i(TAG, "Temporizador pausado ao sair da activity.")
         }
     }
 }
