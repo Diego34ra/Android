78c69802f4fd02167aec45adb1188ace42224742
Diego Ribeiro <102061960+Diego34ra@users.noreply.github.com>
2024-10-07 10:40:17 -0300

    07-10-2024 v3

--- Cervejaria/app/src/main/java/com/ifgoiano/cervejaria/MainActivity.kt
+++ Cervejaria/app/src/main/java/com/ifgoiano/cervejaria/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.ifgoiano.cervejaria
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -13,11 +14,12 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     private lateinit var expertCerveja: ExpertCerveja
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(R.layout.activity_linear_layout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.linearLayout)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
